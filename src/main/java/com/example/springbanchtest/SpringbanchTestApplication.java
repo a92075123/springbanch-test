ce01370ac083a102995d29e2d5049cdf667d25d1
a92075123 <a92075123@gmail.com>
2023-10-15 06:05:47 +0100

    2023/10/15

--- src/main/java/com/example/springbanchtest/SpringbanchTestApplication.java
+++ src/main/java/com/example/springbanchtest/SpringbanchTestApplication.java
@@ -1,13 +1,51 @@
 package com.example.springbanchtest;
 
+import com.example.springbanchtest.tool.TwilioConfig;
+import com.twilio.Twilio;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringbanchTestApplication {
 
+
+
+
+
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbanchTestApplication.class, args);
+
+
+
+
+		ApplicationContext context = SpringApplication.run(SpringbanchTestApplication.class, args);
+
+		JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+		Job job = context.getBean("testJob1", Job.class);
+
+
+		JobParameters params = new JobParametersBuilder()
+				.addString("JobID", String.valueOf(System.currentTimeMillis()))
+				.toJobParameters();
+
+		try {
+			JobExecution jobExecution = jobLauncher.run(job,params);
+			System.out.println("Job Status : " + jobExecution.getStatus());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Job failed");
+		}
+
 	}
 
 }
--- src/test/java/com/example/springbanchtest/SpringbanchTestApplicationTests.java
+++ src/test/java/com/example/springbanchtest/SpringbanchTestApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.springbanchtest;
 
+import com.example.springbanchtest.Job.reader.DbReader;
+import com.example.springbanchtest.bean.Emp;
+import com.example.springbanchtest.mybatis.TestMyBatis01;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.batch.MyBatisCursorItemReader;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@Configuration
 class SpringbanchTestApplicationTests {
 
+
+	@Autowired
+	private DbReader dbReader;
+
 	@Test
-	void contextLoads() {
+	void DbReader() throws Exception {
+
+
+
+		MyBatisCursorItemReader<Emp> reader = dbReader.myItemReader();
+
+		Emp item;
+
+		while ((item = reader.read()) != null) {
+
+			System.out.println(item.getId());
+		}
+
 	}
 
 }
